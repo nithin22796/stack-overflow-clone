9a860c07b6738b4c490c5bde9d1bb89d8b12f152
Nithin Titus <nithin22796@gmail.com>
2024-08-11 13:00:28 -0500

    Model - Client Configuration

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const DB = "my-stack-overflow";
+export const QUESTION_COLLECTION = "questions";
+export const ANSWER_COLLECTION = "answers";
+export const COMMENT_COLLECTION = "comments";
+export const VOTE_COLLECTION = "votes";
+export const ATTACHMENT_BUCKET = "attachment-buckets";
